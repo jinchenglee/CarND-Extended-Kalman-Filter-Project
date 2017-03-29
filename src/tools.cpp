613c332783177da33d36a9a41d31dde9598a331c
jinchenglee <jincheng.li@gmail.com>
2017-03-28 22:16:03 -0700

    Implemented CalculateRMSE() function in tools class.

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tools.h"
+#include <assert.h>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -12,9 +13,30 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
+    *
+    * RMSE = SQRT(Sigma((estimation - ground_truth)^2))/num_samples
+    *
   */
+  // Data sizes
+  int est_size = estimations.size();
+  int gr_size = ground_truth.size();
+  assert (est_size == gr_size);
+  std::cout << "Total number of estimations: " << est_size << "\n";
+
+  // RMSE
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  for(int i=0; i< est_size; i++) {
+    VectorXd tmp;
+    // Element-wise operations
+    tmp = estimations[i] - ground_truth[i];
+    tmp = (tmp.array()*tmp.array());
+    // Accumulation into rmse
+    rmse += tmp;
+  }
+  rmse = rmse.array()/est_size;
+  return rmse.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
